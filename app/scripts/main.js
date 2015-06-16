4362d03b876ef032bc527d76d23ecf8317691812
Alex Law <alexlaw17@gmail.com>
2015-06-16 03:13:43 -0400

    designing layout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,6 +7,7 @@ $(function() {
 		main.toggleClass('sliding-navbar-active');
 		$('.frame').toggleClass('frame-active');
 		$('.hamburger').toggleClass('active');
+		$('.shade').toggleClass('shade-active')
 	});
 
 	// WoW JS http://mynameismatthieu.com/WOW/docs.html
