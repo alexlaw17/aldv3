0716cf1d83bdfeeae6e8d0fd9301779780fb71ec
Alex Law <alexlaw17@gmail.com>
2015-06-16 20:01:27 -0400

    big commit lots of new stuff added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,36 @@
 $(function() {
-	//Navigation Bar Toggle
-	var toggle = $('.sliding-navbar-toggle');
-	var main = $('.sliding-navbar-main');
+    $(window).load(function() {
+        $("#loader").delay(3000).fadeOut('slow');
+    });
 
-	toggle.click(function() {
-		main.toggleClass('sliding-navbar-active');
-		$('.frame').toggleClass('frame-active');
-		$('.hamburger').toggleClass('active');
-		$('.shade').toggleClass('shade-active')
-	});
+    //Smooth Scroll
+    //CSS Tricks Code Snippet - https://css-tricks.com/snippets/jquery/smooth-scrolling/
+    function smoothScroll() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    }
+    smoothScroll();
 
-	// WoW JS http://mynameismatthieu.com/WOW/docs.html
-	// WOW().init();
+    //Navigation Bar Toggle
+    var toggle = $('.sliding-navbar-toggle');
+    var main = $('.sliding-navbar-main');
+
+    toggle.click(function() {
+        main.toggleClass('sliding-navbar-active');
+        $('.frame').toggleClass('frame-active');
+        $('.hamburger').toggleClass('active');
+    });
+
+    // WoW JS http://mynameismatthieu.com/WOW/docs.html
+    // WOW().init();
 });
